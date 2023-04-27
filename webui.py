dc094f7edb74efe3e64b8e1fa94eb3aeda032b81
SmileSmith <longde_chen@163.com>
2023-04-27 13:45:51 +0800

    feat: 支持.env配置

--- webui.py
+++ webui.py
@@ -1,11 +1,15 @@
 from llama_index import SimpleDirectoryReader, LangchainEmbedding, GPTListIndex,GPTSimpleVectorIndex, PromptHelper, LLMPredictor, ServiceContext
 from langchain import OpenAI
-import gradio as gr
+from dotenv.main import load_dotenv
+import gradio
 import sys
 import os
 
-os.chdir(r'D:\ai')  # 文件路径
-os.environ["OPENAI_API_KEY"] = '输入你的chatgpt的api'
+load_dotenv()
+
+favorite_language = os.environ['LANGUAGE']
+openai_api_key = os.environ['OPENAI_API_KEY']
+docs_dir = os.environ['DOC_DIRS']
 
 def construct_index(directory_path):
     max_input_size = 4096
@@ -13,7 +17,7 @@ def construct_index(directory_path):
     max_chunk_overlap = 20
     chunk_size_limit = 600
     prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
-    llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.7, model_name="gpt-3.5-turbo", max_tokens=num_outputs))
+    llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.7, model_name="gpt-3.5-turbo", max_tokens=num_outputs, api_key=openai_api_key))
     documents = SimpleDirectoryReader(directory_path).load_data()
     service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor)
     index = GPTSimpleVectorIndex.from_documents(documents,service_context=service_context)
@@ -25,10 +29,10 @@ def chatbot(input_text):
     response = index.query(input_text, response_mode="compact")
     return response.response
 
-iface = gr.Interface(fn=chatbot,
-                     inputs=gr.inputs.Textbox(lines=7, label="输入您的文本"),
+webui = gradio.Interface(fn=chatbot,
+                     inputs=gradio.inputs.Textbox(lines=7, label="输入您的文本"),
                      outputs="text",
                      title="AI 知识库聊天机器人")
 
-index = construct_index("docs")
-iface.launch(share=True)
+index = construct_index(docs_dir)
+webui.launch(share=True)
